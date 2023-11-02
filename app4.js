2994c30ad838a663f8af9be4206f7dbb53207b2d
chansak04 <deawchansak@gmail.com>
2023-11-02 14:49:24 +0700

    master

---
+++ app4.js
@@ -0,0 +1,7 @@
+let apple = 10 
+{
+    console.log(apple+5) //
+    // document.getElementById('app').innerHTML = apple;
+}
+console.log(apple + 10)//
+document.getElementById('app').innerHTML = apple + 10;
---
+++ app5.js
@@ -0,0 +1,22 @@
+// number 
+const count = 50;
+const bule = 0x000ff;//16
+const umask = 0o0022;//Octal
+const roomTemp = 3.5;//
+const c = 3.0e6
+const nan = NaN //Not a Number
+const message = "Friday"
+const number1 = "12345"
+const myString = "Hello"
+const isMary = true
+console.log(typeof isMary);
+console.log(myString[6]);
+console.log(count);
+console.log(bule);
+console.log(umask);
+console.log(typeof roomTemp);
+console.log(c);
+console.log(nan);
+console.log(typeof message);
+console.log(typeof number1);
+document.getElementById("app").innerHTML = count
